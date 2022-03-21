a554e9c2708622545f00cfea106073a1237934d4
hellobrother1472 <101651107+hellobrother1472@users.noreply.github.com>
2022-03-21 20:48:21 +0530

    Initial File Upload

---
+++ index.js
@@ -0,0 +1,66 @@
+function selected() {
+    // var music = new Audio("sounds/tom-1.mp3");
+    // music.play();
+    var buttonInnerHtml = this.innerHTML;
+    makesound(buttonInnerHtml);
+    addAnimation(buttonInnerHtml);
+}
+
+
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+    }
+}
+
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", selected);
+}
+
+document.addEventListener("keydown", function (event) {
+    makesound(event.key);
+    addAnimation(event.key);
+});
+
+function addAnimation(keypressed) {
+    var btn = document.querySelector("." + keypressed);
+    btn.classList.add("pressed");
+
+    setTimeout(function(){
+
+        btn.classList.remove("pressed");
+
+    },100);
+    
+}
